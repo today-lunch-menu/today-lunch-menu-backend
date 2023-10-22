4c0cabeec6ac9c4957601b98bb67b5e178bc5f69
partykyoung <partykyoung@gmail.com>
2023-10-23 01:46:20 +0900

    코틀린, 스프링 초기 세팅

---
+++ src/main/kotlin/com/wonyangEoseon/todayMealMenu/TodayMealMenuApplication.kt
@@ -0,0 +1,11 @@
+package com.wonyangEoseon.todayMealMenu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodayMealMenuApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodayMealMenuApplication>(*args)
+}
